da1c9f1ecff2547524ffdee5f5ac06a7cfc85647
midhula <pravalika2348@gmail.com>
2026-01-28 16:37:01 +0530

    Use Reducer

---
+++ Frontened/src/hooks/Hook.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+const Hook = () => {
+  return (
+    <div>
+        <ol>
+            <li><Link to ='useState'>useState</Link></li>
+            <li><Link to ='useEffect'>useEffect</Link></li>
+            <li><Link to ='useEffectApi'>useEffectwithApi</Link></li>
+            <li><Link to ='UseRef'>useRef</Link></li>
+            <li><Link to ='UseReducer'>useReducer</Link></li>
+            
+        </ol>
+        <Outlet/>
+
+    </div>
+  )
+}
+
+export default Hook
